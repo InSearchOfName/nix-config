e738d24055b4490431f52ab499664dace3036366
InSearchOfName <128755553+InSearchOfName@users.noreply.github.com>
2026-07-03 15:26:08 +0200

    remove default password

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -4,7 +4,6 @@
   users.users.insearchofname = {
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ];
-    initialPassword = "changeme";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvV6m3GVXnXsGxc5CJf54NJT8oywAnUmE1PBnujRnwo tommy@Holo"
     ];
